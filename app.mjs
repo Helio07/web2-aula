0ff311f9274e8cc0160dd556e7373c514f3a8003
Helio07 <heliojuniorsette73@gmail.com>
2024-04-09 15:19:50 -0300

    Aula-09-04

--- app.mjs
+++ app.mjs
@@ -5,14 +5,16 @@ import veiculo from './models/veiculo.mjs'
 const app = express()
 const port = 3000
 
+app.use(express.urlencoded())
+app.use(express.json())
+
 app.get('/veiculos', async (req, res) => {
     res.json(await veiculo.findAll())
 })
 
 app.post('/veiculos', async (req, res) => {
-    const  created = await veiculo.create({fabricante: "GM", modelo: "Corsa"});
-
-    res.send(created)
+    console.log(req.body)
+   
 });
 
 app.listen(port, () => {
